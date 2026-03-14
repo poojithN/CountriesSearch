cd0646d30e2928bcfaaf699534cdb17af47bd734
poojith <you@example.com>
2026-03-14 21:38:32 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css';
+import React from 'react';
+import { useEffect,useState } from 'react';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(()=>{
+    fetch(" https://countries-search-data-prod-812920491762.asia-south1.run.app/countries")
+    .then((response) => response.json())
+    .then((data) => {
+        setCountries(data);
+      })
+   .catch((error)=>console.error("Error fetching data:", error))
+},[]);
+
+ const filteredCountries = countries.filter((country) =>
+    country.common.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <>
+    <div>
+      <center> <input
+        type="text"
+        placeholder="Search for countries"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      /></center>
+    </div>
+    <div className="App">
+      {filteredCountries.map((country, i) => (
+        <div key={i} className='c-card'>
+          <img
+            src={country.png}
+            alt={country.name}
+            className='c-flag'
+          />
+          <p className='c-name'>{country.common}</p>
+          </div>
+      ))}
+    </div>
+    </>
+  );
+}
+
+export default App;
